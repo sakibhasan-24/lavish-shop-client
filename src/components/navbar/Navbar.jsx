e3eb275bb2826db56e860b0ae9090961015ec5de
sakibhasan-24 <sakibhasan7724@gmail.com>
2024-07-04 18:51:25 +0600

    header update

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -10,10 +10,12 @@ import {
 } from "@ant-design/icons";
 import { Button, Drawer } from "antd";
 import { useState } from "react";
+import { useSelector } from "react-redux";
 
 export default function Navbar() {
   const [drawerVisible, setDrawerVisible] = useState(false);
-
+  const { cartItem } = useSelector((state) => state.cart);
+  console.log(cartItem);
   const showDrawer = () => {
     setDrawerVisible(true);
   };
@@ -45,6 +47,35 @@ export default function Navbar() {
           <ShoppingOutlined />
           Categories
         </Link>
+        {cartItem.length > 0 && (
+          <Link to="">
+            <div
+              tabIndex={0}
+              role="button"
+              className="text-md font-semibold text-white hover:text-yellow-300 transition-colors duration-300 flex items-center gap-2"
+            >
+              <div className="indicator">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-5 w-5"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
+                  />
+                </svg>
+                <span className="badge badge-sm indicator-item">
+                  {cartItem.length}
+                </span>
+              </div>
+            </div>
+          </Link>
+        )}
         <Link
           to="/offers"
           className="text-md font-semibold text-white hover:text-yellow-300 transition-colors duration-300 flex items-center gap-2"
